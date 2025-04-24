e00258e7a7d2eaa26f866cc13e9a81aac4088b6a
eclopez <erik@eriklopez.net>
2025-04-23 22:41:03 -0700

    style: Add theme font, font sizing, and colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
 
 import './global.css';
 
@@ -8,11 +9,18 @@ const metadata: Metadata = {
   description: `Task Nerd manages your tasks so you don't have to.`,
 };
 
+const montserrat = Montserrat({
+  weight: ['300', '500', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-montserrat',
+});
+
 function RootLayout(props: React.PropsWithChildren) {
   const { children } = props;
 
   return (
-    <html lang="en">
+    <html lang="en" className={`${montserrat.variable}`}>
       <body>{children}</body>
     </html>
   );
