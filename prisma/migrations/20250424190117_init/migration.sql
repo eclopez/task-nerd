77710abc4dc72d6c3c58a3088b8ff9ca9bb40a45
eclopez <erik@eriklopez.net>
2025-04-25 01:35:44 -0700

    refactor: Change the data model

--- prisma/migrations/20250424190117_init/migration.sql
+++ prisma/migrations/20250424190117_init/migration.sql
@@ -12,7 +12,6 @@ CREATE TABLE "Task" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT,
-    "completed" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3),
     "completedAt" TIMESTAMP(3),
