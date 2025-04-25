de25e63b6600f04646a6d942acaabcc0fd6afcd8
eclopez <erik@eriklopez.net>
2025-04-25 12:32:46 -0700

    feat: Add links to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,18 @@
+import Link from 'next/link';
+import { Button } from '@mui/material';
+
 function Header() {
   return (
-    <header className="align-center flex flex-row justify-start px-4 py-2">
+    <header className="align-center flex flex-row justify-between px-4 py-2">
       <h1 className="text-heading-1 text-primary-100 font-sans font-bold">
-        task
-        <span className="text-primary-900 -ml-1">nerd</span>
+        <Link href="/">
+          task
+          <span className="text-primary-900 -ml-1">nerd</span>
+        </Link>
       </h1>
+      <Button type="button" variant="contained" color="secondary">
+        <Link href="/task">Create a task</Link>
+      </Button>
     </header>
   );
 }
