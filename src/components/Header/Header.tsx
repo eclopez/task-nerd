24c4d03cbf5cbe1f19caf0eaa818b1708b33a3d3
eclopez <erik@eriklopez.net>
2025-04-25 01:17:45 -0700

    style: Change size of heading

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="align-center flex flex-row justify-start px-4 py-2">
-      <h1 className="text-heading-3 text-primary-100 font-sans font-bold">
+      <h1 className="text-heading-1 text-primary-100 font-sans font-bold">
         task
         <span className="text-primary-900 -ml-1">nerd</span>
       </h1>
